b9237b859f45c88b15fc4ddecc8ab063db0ff373
reysar25 <reyhanaintisaar@gmail.com>
2025-03-22 22:47:49 +0300

    index html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // Your code here
+function displayRamens() {
+    const imagesDiv = document.getElementById("images");
+    imagesDiv.innerHTML = ""; 
+
+
+img.addEventListener("click", () => {
+    console.log(`Clicked on: ${ramen.name}`);
+    displayRamenDetails(ramen);
+});
+
+
+Document.querySelectorAll('th').forEach(th => {
+    th.addEventListener('click', function() {
+        const gifUrl = this.getAttribute('data-gif');
+        const gifDisplay = document.getElementById('gif-display');
+        gifDisplay.src = gifUrl;
+        gifDisplay.style.display = 'block';
+    });
+});
